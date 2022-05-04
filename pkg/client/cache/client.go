a2ebd09c7391dd2beca25ee9d5f7f3467d536659
LuaSavage <ilya.shikri@gmail.com>
2022-05-04 16:06:12 +0300

    redis client has been wrapped

---
+++ pkg/client/cache/client.go
@@ -0,0 +1,25 @@
+package cache
+
+import (
+	"context"
+
+	redis "github.com/go-redis/redis/v8"
+)
+
+type Client interface {
+	Pipelined(ctx context.Context, fn func(redis.Pipeliner) error) ([]redis.Cmder, error)
+	HSet(ctx context.Context, key string, values ...interface{}) *redis.IntCmd
+	HGetAll(ctx context.Context, key string) *redis.StringStringMapCmd
+	HMGet(ctx context.Context, key string, fields ...string) *redis.SliceCmd
+}
+
+func NewClient(host string, password string, db int) (Client, error) {
+
+	newClient := redis.NewClient(&redis.Options{
+		Addr:     host,
+		Password: "",
+		DB:       db,
+	})
+
+	return newClient, nil
+}
