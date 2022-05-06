e2744ae05656b7a07149d1ab362d13b26bbbebc4
LuaSavage <ilya.shikri@gmail.com>
2022-05-06 17:01:10 +0300

    storage module succesfully covered with test

--- internal/domain/video/storage_test.go
+++ internal/domain/video/storage_test.go
@@ -3,6 +3,7 @@ package video
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"testing"
 
 	miniredis "github.com/alicebob/miniredis"
@@ -25,21 +26,32 @@ func newTestRedis() (*redis.Client, *redismock.ClientMock) {
 	return client, redismock.NewNiceMock(client)
 }
 
-func TestGetVideoByID(t *testing.T) {
+// initialise test object
+var (
+	testVideo Video = Video{
+		Title:       "Test video",
+		Id:          "=TestvdIeo",
+		PublishTime: "22-09-2011",
+		Channel:     "someshit",
+		Views:       "228k",
+		Thumbnail:   "htttp://sdf.dickpic",
+	}
+	ctx context.Context = context.TODO()
+)
+
+func TestGetVideoByIDError(t *testing.T) {
+	t.Run("error due to empty video object response", func(t *testing.T) {
+		redisClient, _ := newTestRedis()
+		videoStorage := NewStorage(redisClient)
+		retrivedVideo, err := videoStorage.GetVideoByID(ctx, testVideo.Id)
+		assert.Error(t, err, fmt.Sprintf("and retrived video looks like %+v\n", retrivedVideo))
+	})
+}
+
+func TestGetVideoByIDOK(t *testing.T) {
 	t.Run("error due to test video doesnt received", func(t *testing.T) {
 		redisClient, redisMock := newTestRedis()
 		videoStorage := NewStorage(redisClient)
-		ctx := context.TODO()
-
-		// initialise test object
-		testVideo := Video{
-			Title:       "Test video",
-			Id:          "=TestvdIeo",
-			PublishTime: "22-09-2011",
-			Channel:     "someshit",
-			Views:       "228k",
-			Thumbnail:   "htttp://sdf.dickpic",
-		}
 
 		// writing test video into
 		var videoMaped map[string]string
@@ -51,7 +63,37 @@ func TestGetVideoByID(t *testing.T) {
 			redisMock.HSet(ctx, "video:"+testVideo.Id, key, value)
 		}
 
-		retrivenVideo, err := videoStorage.GetVideoByID(context.Background(), testVideo.Id)
+		retrivenVideo, err := videoStorage.GetVideoByID(ctx, testVideo.Id)
 		assert.Equal(t, testVideo, *retrivenVideo, err)
+	})
+}
+
+func TestCreateVideoOK(t *testing.T) {
+	t.Run("error due to the video  dosn't properly created", func(t *testing.T) {
+		redisClient, _ := newTestRedis()
+		videoStorage := NewStorage(redisClient)
+
+		_, err := videoStorage.GetVideoByID(ctx, testVideo.Id)
+		assert.Error(t, err)
+
+		err = videoStorage.CreateVideo(ctx, testVideo)
+		assert.NoError(t, err)
+
+		// If it succesfully written then it has to be same as origin
+		retrivenVideo, err := videoStorage.GetVideoByID(ctx, testVideo.Id)
+		assert.Equal(t, testVideo, *retrivenVideo, err)
+	})
+}
+
+// prevention of double write issue
+func TestCreateVideoError(t *testing.T) {
+	t.Run("error due to double storring video up issue", func(t *testing.T) {
+		redisClient, _ := newTestRedis()
+		videoStorage := NewStorage(redisClient)
+
+		err := videoStorage.CreateVideo(ctx, testVideo)
+		assert.NoError(t, err)
+		err = videoStorage.CreateVideo(ctx, testVideo)
+		assert.Error(t, err)
 	})
 }
