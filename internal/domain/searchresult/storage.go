dc5af92306968143dc03cc1ebdf1089469e380c8
LuaSavage <ilya.shikri@gmail.com>
2022-05-06 19:59:32 +0300

    fixed get/set actions

--- internal/domain/searchresult/storage.go
+++ internal/domain/searchresult/storage.go
@@ -10,7 +10,7 @@ import (
 )
 
 type Storage interface {
-	GetSearchResultByQuary(ctx context.Context, query string) (*SearchResult, error)
+	GetSearchResultByQuary(ctx context.Context, query string) (*StoreSearchResultDTO, error)
 	CreateSearchResult(ctx context.Context, searchResult *SearchResult) error
 }
 
@@ -22,39 +22,48 @@ func NewStorage(client cache.Client) Storage {
 	return &storage{client: client}
 }
 
-func (s *storage) GetSearchResultByQuary(ctx context.Context, query string) (*SearchResult, error) {
-
-	var searchResult SearchResult
+func (s *storage) GetSearchResultByQuary(ctx context.Context, query string) (*StoreSearchResultDTO, error) {
+	var searchResultDTO StoreSearchResultDTO
 
 	// get search result hash by type:id from redis
-	if err := s.client.HGetAll(ctx, "search_result:"+query).Scan(&searchResult); err != nil {
+	if err := s.client.HGetAll(ctx, "search_result:"+query).Scan(&searchResultDTO); err != nil {
 		return nil, err
 	}
 
-	return nil, fmt.Errorf("search results by query '%s' alrady exists", query)
+	if searchResultDTO.Query != query {
+		return nil, fmt.Errorf("search results by query '%s' does'nt exists", query)
+	}
+
+	return &searchResultDTO, nil
 }
 
 // put it in a cache
 func (s *storage) CreateSearchResult(ctx context.Context, searchResult *SearchResult) error {
 
 	if _, err := s.GetSearchResultByQuary(ctx, searchResult.Query); err == nil {
-		return err
+		return fmt.Errorf("search result bu queary '%s' already exists in cache", searchResult.Query)
 	}
 
 	// trying to write it into redis
-	if _, err := s.client.Pipelined(ctx, func(rdb redis.Pipeliner) error {
+	_, err := s.client.Pipelined(ctx, func(rdb redis.Pipeliner) error {
 
 		// dto contains slice of video.id in place  of video models
 		searchResultDTO := NewStoreSearchResultDTO(*searchResult)
-		rdb.HSet(ctx, "search_result:"+searchResultDTO.Query, "query", searchResultDTO.Query)
+		cmd := rdb.HSet(ctx, "search_result:"+searchResultDTO.Query, "query", searchResultDTO.Query)
+
+		if cmd.Err() != nil {
+			return cmd.Err()
+		}
 
 		data, _ := json.Marshal(searchResultDTO.Videos)
-		rdb.HSet(ctx, "search_result:"+searchResultDTO.Query, "videos", data)
+		cmd = rdb.HSet(ctx, "search_result:"+searchResultDTO.Query, "videos", data)
+
+		if cmd.Err() != nil {
+			return cmd.Err()
+		}
 
 		return nil
-	}); err != nil {
-		return err
-	}
+	})
 
-	return nil
+	return err
 }
