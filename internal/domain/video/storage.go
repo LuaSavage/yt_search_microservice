689659ad7e3dd1bb904be90b02007cc643b4508c
LuaSavage <ilya.shikri@gmail.com>
2022-05-06 16:59:12 +0300

    a get/set issues were solved

--- internal/domain/video/storage.go
+++ internal/domain/video/storage.go
@@ -3,6 +3,7 @@ package video
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 
 	cache "github.com/LuaSavage/yt_search_microservice/pkg/client/cache"
 	redis "github.com/go-redis/redis/v8"
@@ -30,27 +31,35 @@ func (s *storage) GetVideoByID(ctx context.Context, id string) (*Video, error) {
 		return nil, err
 	}
 
+	if video.Id != id {
+		return nil, fmt.Errorf("retriven video holds inproper id or none. video id: %s", video.Id)
+	}
+
 	return video, nil
 }
 
 func (s *storage) CreateVideo(ctx context.Context, video Video) error {
 
 	if _, err := s.GetVideoByID(ctx, video.Id); err == nil {
-		return err
+		return fmt.Errorf("video with id '%s' already exists in cache", video.Id)
 	}
+
+	var videoMaped map[string]interface{}
+	data, _ := json.Marshal(video)
+	json.Unmarshal(data, &videoMaped)
 
 	// trying to write it into redis
 	if _, err := s.client.Pipelined(ctx, func(rdb redis.Pipeliner) error {
-		var videoMaped map[string]interface{}
-
-		data, _ := json.Marshal(video)
-		json.Unmarshal(data, &videoMaped)
 
 		for key, value := range videoMaped {
-			rdb.HSet(ctx, "video:"+video.Id, key, value)
+			cmd := rdb.HSet(ctx, "video:"+video.Id, key, value)
+			if cmd.Err() != nil {
+				return cmd.Err()
+			}
 		}
 
 		return nil
+
 	}); err != nil {
 		return err
 	}
